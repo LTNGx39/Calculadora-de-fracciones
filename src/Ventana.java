260a00c0eaea01a0f85e771bde7bc8691ccd13f2
LTNGx39 <131133076+LTNGx39@users.noreply.github.com>
2024-11-20 12:28:29 -0600

    Update graphics - Añade ventana simple - Modifica main para mostrar la ventana

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,8 +1,31 @@
-package src;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Ventana donde se ve la ejecucion principal del programa
  */
+
 public class Ventana extends javax.swing.JFrame {
 
+    int usableWidth, usableHeight;
+
+    // Constructor para la ventana
+    public Ventana() {
+
+        // Tamaño de pantalla
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        screen.setSize(screen.width / 3, screen.width / 4);
+
+        // Configuracion de ventana
+        setSize(screen);
+        setLocationRelativeTo(null);
+        setUndecorated(true);
+        setContentPane(new RoundPane(this));
+        setBackground(new Color(0, 0, 0, 0));
+
+        // Llenado de variables
+        usableWidth = getWidth() - 2;
+        usableHeight = getHeight() - 2;
+
+    }
 }
