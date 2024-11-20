da26393decd69902b8dd4e5a8569c0a0f74fc24a
LTNGx39 <131133076+LTNGx39@users.noreply.github.com>
2024-11-20 12:19:10 -0600

    Add Color.java - Clase que guarda la paleta de colores

---
+++ src/Colors.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+/**
+ * Clase que almacena la paleta de colores usada en el programa
+ */
+
+public class Colors {
+    
+    static final Color BLACK = new Color(30, 30, 30);
+    static final Color DARK_GRAY = new Color(44, 44, 44);
+    static final Color GRAY = new Color(67, 67, 67);
+    static final Color WHITE = new Color(245, 245, 245);
+    static final Color RED = new Color(255, 45, 85);
+    static final Color BLUE = new Color(48, 176, 199);
+
+}
