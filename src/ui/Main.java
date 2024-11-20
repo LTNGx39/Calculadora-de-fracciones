3f596484d0eb8d93745412c55a744da4790fb984
LTNGx39 <131133076+LTNGx39@users.noreply.github.com>
2024-11-20 12:46:50 -0600

    Move files to ui package

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,11 @@
+package ui;
+
 public class Main {
     
     public static void main(String[] args) {
 
         Ventana ventana = new Ventana();
         ventana.setVisible(true);
-        
+
     }
 }
