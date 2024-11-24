82a5c94a6a2f492aa664ce9a00996a36a0ffea48
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-24 15:56:24 -0600

    updating code

---
+++ src/Fractions.java
@@ -0,0 +1,11 @@
+public class Fractions {
+    protected int numerator;
+    protected int denominator;
+
+    public Fractions(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    
+}
