274feb9e9be06e336dde5efcddd8591fc0ebcf9d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-24 19:41:10 -0600

    ..

---
+++ src/Fraction.java
@@ -0,0 +1,43 @@
+public class Fraction {
+    public Integer numerator;
+    public Integer denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Integer getNumerator() {
+        return this.numerator;
+    }
+
+    public Integer getDenominator() {
+        return this.denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public Fraction simplify(Fraction fraction) {
+        int gcd = this.gcd(fraction.getNumerator(), fraction.getDenominator());
+        fraction.setNumerator(fraction.getNumerator() / gcd);
+        fraction.setDenominator(fraction.getDenominator() / gcd);
+        return fraction;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public String toString(boolean sign, boolean parenthesis) {
+        return this.numerator + "/" + this.denominator;
+    }
+}
