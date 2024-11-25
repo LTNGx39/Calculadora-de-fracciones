43d08f4356cb3e212479a32581e298f97d708ae0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-24 19:59:05 -0600

    ping

--- src/logic/Fractions.java
+++ src/logic/Fractions.java
@@ -1,24 +1,8 @@
 package logic;
 
 public class Fractions {
-    
-    protected int numerator;
-    protected int denominator;
 
-    public Fractions(int numerator, int denominator) {
-        this.numerator = numerator;
-        this.denominator = denominator;
-    }
-
-    public int getNumerator() {
-        return numerator;
-    }
-
-    public int getDenominator() {
-        return denominator;
-    }
-
-    public Fraction suma(Fraction fractionA, Fraction fractionB) {
+    public Fraction add(Fraction fractionA, Fraction fractionB) {
         Fraction result = new Fraction(0, 0);
         result.numerator = fractionA.numerator * fractionB.denominator + fractionB.numerator * fractionA.denominator;
         result.denominator = fractionA.denominator * fractionB.denominator;
