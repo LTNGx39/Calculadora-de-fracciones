aef55abd8e26e479c4b77215397f4ea9e6a0c2a8
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-24 21:02:42 -0600

    adding simplify() method call to every operation

--- src/logic/Fractions.java
+++ src/logic/Fractions.java
@@ -6,27 +6,27 @@ public class Fractions {
         Fraction result = new Fraction(0, 0);
         result.numerator = fractionA.numerator * fractionB.denominator + fractionB.numerator * fractionA.denominator;
         result.denominator = fractionA.denominator * fractionB.denominator;
-        return result;
+        return result.simplify();
     }
 
     public static Fraction resta(Fraction fractionA, Fraction fractionB) {
         Fraction result = new Fraction(0, 0);
         result.numerator = fractionA.numerator * fractionB.denominator - fractionB.numerator * fractionA.denominator;
         result.denominator = fractionA.denominator * fractionB.denominator;
-        return result;
+        return result.simplify();
     }
 
     public static Fraction multiplicacion(Fraction fractionA, Fraction fractionB) {
         Fraction result = new Fraction(0, 0);
         result.numerator = fractionA.numerator * fractionB.numerator;
         result.denominator = fractionA.denominator * fractionB.denominator;
-        return result;
+        return result.simplify();
     }
 
     public static Fraction division(Fraction fractionA, Fraction fractionB) {
         Fraction result = new Fraction(0, 0);
         result.numerator = fractionA.numerator * fractionB.denominator;
         result.denominator = fractionA.denominator * fractionB.numerator;
-        return result;
+        return result.simplify();
     }
 }
