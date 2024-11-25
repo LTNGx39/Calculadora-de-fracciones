bf5e0537d1314da1b949a8c8a446a23db25871c2
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-24 20:31:56 -0600

    inglish pikinglish

--- src/logic/Fractions.java
+++ src/logic/Fractions.java
@@ -2,7 +2,7 @@ package logic;
 
 public class Fractions {
 
-    public static Fraction suma(Fraction fractionA, Fraction fractionB) {
+    public static Fraction add(Fraction fractionA, Fraction fractionB) {
         Fraction result = new Fraction(0, 0);
         result.numerator = fractionA.numerator * fractionB.denominator + fractionB.numerator * fractionA.denominator;
         result.denominator = fractionA.denominator * fractionB.denominator;
