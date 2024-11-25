7d1e1f5bcfc0bce6eb7046353a108e486bfa13f0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-11-24 20:33:30 -0600

    added basic operations

--- src/logic/Fractions.java
+++ src/logic/Fractions.java
@@ -8,4 +8,25 @@ public class Fractions {
         result.denominator = fractionA.denominator * fractionB.denominator;
         return result;
     }
+
+    public static Fraction sustract(Fraction fractionA, Fraction fractionB) {
+        Fraction result = new Fraction(0, 0);
+        result.numerator = fractionA.numerator * fractionB.denominator - fractionB.numerator * fractionA.denominator;
+        result.denominator = fractionA.denominator * fractionB.denominator;
+        return result;
+    }
+
+    public static Fraction multiply(Fraction fractionA, Fraction fractionB) {
+        Fraction result = new Fraction(0, 0);
+        result.numerator = fractionA.numerator * fractionB.numerator;
+        result.denominator = fractionA.denominator * fractionB.denominator;
+        return result;
+    }
+
+    public static Fraction divide(Fraction fractionA, Fraction fractionB) {
+        Fraction result = new Fraction(0, 0);
+        result.numerator = fractionA.numerator * fractionB.denominator;
+        result.denominator = fractionA.denominator * fractionB.numerator;
+        return result;
+    }
 }
